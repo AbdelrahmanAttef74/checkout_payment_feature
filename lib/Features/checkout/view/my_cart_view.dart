f48b32b85c3b738fc643e335b524057c35435d2a
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-07-16 15:59:07 +0300

    create app bar

--- lib/Features/checkout/view/my_cart_view.dart
+++ lib/Features/checkout/view/my_cart_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Center(child: SvgPicture.asset('assets/images/arrow.svg')),
+        centerTitle: true,
+        title: const Text(
+          'My Cart ',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
   }
 }
