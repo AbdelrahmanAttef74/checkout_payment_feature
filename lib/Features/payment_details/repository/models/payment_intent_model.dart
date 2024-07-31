257d1ce569efc6da015aa731436164fd4dcc7b92
AbdelrahmanAttef74 <abdoeldox@gmail.com>
2024-07-31 11:45:06 +0300

    refactor payment intent model

--- lib/Features/payment_details/repository/models/payment_intent_model.dart
+++ lib/Features/payment_details/repository/models/payment_intent_model.dart
@@ -1,3 +1,15 @@
+// To parse this JSON data, do
+//
+//     final intentPaymentModel = intentPaymentModelFromJson(jsonString);
+
+import 'dart:convert';
+
+PaymentIntentModel intentPaymentModelFromJson(String str) =>
+    PaymentIntentModel.fromJson(json.decode(str));
+
+String intentPaymentModelToJson(PaymentIntentModel data) =>
+    json.encode(data.toJson());
+
 class PaymentIntentModel {
   String? id;
   String? object;
@@ -80,6 +92,94 @@ class PaymentIntentModel {
     this.transferData,
     this.transferGroup,
   });
+
+  factory PaymentIntentModel.fromJson(dynamic json) => PaymentIntentModel(
+        id: json["id"],
+        object: json["object"],
+        amount: json["amount"],
+        amountCapturable: json["amount_capturable"],
+        amountDetails: AmountDetails.fromJson(json["amount_details"]),
+        amountReceived: json["amount_received"],
+        application: json["application"],
+        applicationFeeAmount: json["application_fee_amount"],
+        automaticPaymentMethods:
+            AutomaticPaymentMethods.fromJson(json["automatic_payment_methods"]),
+        canceledAt: json["canceled_at"],
+        cancellationReason: json["cancellation_reason"],
+        captureMethod: json["capture_method"],
+        clientSecret: json["client_secret"],
+        confirmationMethod: json["confirmation_method"],
+        created: json["created"],
+        currency: json["currency"],
+        customer: json["customer"],
+        description: json["description"],
+        invoice: json["invoice"],
+        lastPaymentError: json["last_payment_error"],
+        latestCharge: json["latest_charge"],
+        livemode: json["livemode"],
+        metadata: Metadata.fromJson(json["metadata"]),
+        nextAction: json["next_action"],
+        onBehalfOf: json["on_behalf_of"],
+        paymentMethod: json["payment_method"],
+        paymentMethodOptions:
+            PaymentMethodOptions.fromJson(json["payment_method_options"]),
+        paymentMethodTypes:
+            List<String>.from(json["payment_method_types"].map((x) => x)),
+        processing: json["processing"],
+        receiptEmail: json["receipt_email"],
+        review: json["review"],
+        setupFutureUsage: json["setup_future_usage"],
+        shipping: json["shipping"],
+        source: json["source"],
+        statementDescriptor: json["statement_descriptor"],
+        statementDescriptorSuffix: json["statement_descriptor_suffix"],
+        status: json["status"],
+        transferData: json["transfer_data"],
+        transferGroup: json["transfer_group"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "object": object,
+        "amount": amount,
+        "amount_capturable": amountCapturable,
+        "amount_details": amountDetails?.toJson(),
+        "amount_received": amountReceived,
+        "application": application,
+        "application_fee_amount": applicationFeeAmount,
+        "automatic_payment_methods": automaticPaymentMethods?.toJson(),
+        "canceled_at": canceledAt,
+        "cancellation_reason": cancellationReason,
+        "capture_method": captureMethod,
+        "client_secret": clientSecret,
+        "confirmation_method": confirmationMethod,
+        "created": created,
+        "currency": currency,
+        "customer": customer,
+        "description": description,
+        "invoice": invoice,
+        "last_payment_error": lastPaymentError,
+        "latest_charge": latestCharge,
+        "livemode": livemode,
+        "metadata": metadata?.toJson(),
+        "next_action": nextAction,
+        "on_behalf_of": onBehalfOf,
+        "payment_method": paymentMethod,
+        "payment_method_options": paymentMethodOptions?.toJson(),
+        "payment_method_types":
+            List<dynamic>.from(paymentMethodTypes!.map((x) => x)),
+        "processing": processing,
+        "receipt_email": receiptEmail,
+        "review": review,
+        "setup_future_usage": setupFutureUsage,
+        "shipping": shipping,
+        "source": source,
+        "statement_descriptor": statementDescriptor,
+        "statement_descriptor_suffix": statementDescriptorSuffix,
+        "status": status,
+        "transfer_data": transferData,
+        "transfer_group": transferGroup,
+      };
 }
 
 class AmountDetails {
@@ -88,10 +188,22 @@ class AmountDetails {
   AmountDetails({
     this.tip,
   });
+
+  factory AmountDetails.fromJson(Map<String, dynamic> json) => AmountDetails(
+        tip: Metadata.fromJson(json["tip"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "tip": tip?.toJson(),
+      };
 }
 
 class Metadata {
   Metadata();
+
+  factory Metadata.fromJson(Map<String, dynamic> json) => Metadata();
+
+  Map<String, dynamic> toJson() => {};
 }
 
 class AutomaticPaymentMethods {
@@ -100,6 +212,15 @@ class AutomaticPaymentMethods {
   AutomaticPaymentMethods({
     this.enabled,
   });
+
+  factory AutomaticPaymentMethods.fromJson(Map<String, dynamic> json) =>
+      AutomaticPaymentMethods(
+        enabled: json["enabled"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "enabled": enabled,
+      };
 }
 
 class PaymentMethodOptions {
@@ -110,6 +231,17 @@ class PaymentMethodOptions {
     this.card,
     this.link,
   });
+
+  factory PaymentMethodOptions.fromJson(Map<String, dynamic> json) =>
+      PaymentMethodOptions(
+        card: Card.fromJson(json["card"]),
+        link: Link.fromJson(json["link"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "card": card?.toJson(),
+        "link": link?.toJson(),
+      };
 }
 
 class Card {
@@ -124,6 +256,20 @@ class Card {
     this.network,
     this.requestThreeDSecure,
   });
+
+  factory Card.fromJson(Map<String, dynamic> json) => Card(
+        installments: json["installments"],
+        mandateOptions: json["mandate_options"],
+        network: json["network"],
+        requestThreeDSecure: json["request_three_d_secure"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "installments": installments,
+        "mandate_options": mandateOptions,
+        "network": network,
+        "request_three_d_secure": requestThreeDSecure,
+      };
 }
 
 class Link {
@@ -132,4 +278,12 @@ class Link {
   Link({
     this.persistentToken,
   });
+
+  factory Link.fromJson(Map<String, dynamic> json) => Link(
+        persistentToken: json["persistent_token"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "persistent_token": persistentToken,
+      };
 }
